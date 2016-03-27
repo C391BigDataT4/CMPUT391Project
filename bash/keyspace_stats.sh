90c01f2e93cb84997d90cec4de64dc1f418af9d1
Xiaohui Ma <xiaohui6@ualberta.ca>
2016-03-27 14:04:05 -0600

    rm bash folder

--- bash/keyspace_stats.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ssh -i 391winter.pem ubuntu@10.2.3.31 "~/cassandra/apache-cassandra-2.0.5/bin/nodetool cfstats group4"
--- bash/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# upload the necessary files to node 1
-scp -i 391winter.pem ../cql/setup.cql ../cql/columns.cql generate.py ubuntu@10.2.3.31:~
-
-# reset database. This will drop the whole keyspace. All data will be removed.
-ssh -i 391winter.pem ubuntu@10.2.3.31 "/home/ubuntu/cassandra/apache-cassandra-2.0.5/bin/cqlsh -f setup.cql"
