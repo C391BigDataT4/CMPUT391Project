573e468007a6db69561ee0e63a38f25f5298b0d8
Xiaohui Ma <xiaohui6@ualberta.ca>
2016-03-27 14:02:52 -0600

    update

--- generate_data_of_n_days.sh
+++ generate_data_of_n_days.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# upload "generate.py" to node 1
-scp -i 391winter.pem generate.py ubuntu@10.2.3.31:~
-
 echo "Enter the number of days, followed by [ENTER]:"
 read days
 
--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # upload files to node 1
-scp -i 391winter.pem cql/setup.cql cql/columns.cql ubuntu@10.2.3.31:~
+scp -i 391winter.pem cql/setup.cql cql/columns.cql generate.py ubuntu@10.2.3.31:~
 
+# reset database. All data in keyspace "group4" will be remove.
 ssh -i 391winter.pem ubuntu@10.2.3.31 "/home/ubuntu/cassandra/apache-cassandra-2.0.5/bin/cqlsh -f setup.cql"
