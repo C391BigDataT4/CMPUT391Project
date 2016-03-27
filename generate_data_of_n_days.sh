0e9cb2825768a1c810c8c79fd5ac53de6cf24f34
Xiaohui Ma <xiaohui6@ualberta.ca>
2016-03-27 17:08:24 -0600

    update settings

--- generate_data_of_n_days.sh
+++ generate_data_of_n_days.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ssh -i 391winter.pem ubuntu@10.2.3.31 "/home/ubuntu/cassandra/apache-cassandra-2.0.5/bin/cqlsh -f setup.cql"
+
 echo "Enter the number of days, followed by [ENTER]:"
 read days
 
--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
+echo "Stopping the server..."
+sh stop_remote_server.sh
+echo "Server stopped."
+
 # reset database. All data in keyspace "group4" will be remove.
-ssh -i 391winter.pem ubuntu@10.2.3.31 "/home/ubuntu/cassandra/apache-cassandra-2.0.5/bin/cqlsh -f setup.cql"
+ssh -i 391winter.pem ubuntu@10.2.3.31 "rm -rf /mystorage/cassandra/"
+ssh -i 391winter.pem ubuntu@10.2.3.30 "rm -rf /mystorage/cassandra/"
+ssh -i 391winter.pem ubuntu@10.2.3.5 "rm -rf /mystorage/cassandra/"
+
+# upload files to node 1
+echo "Uploading files..."
+scp -i 391winter.pem cql/setup.cql cql/columns.cql generate.py ubuntu@10.2.3.31:/home/ubuntu/
+
+# upload settings to all nodes
+scp -i 391winter.pem conf/cassandra_node_1.yaml ubuntu@10.2.3.31:/home/ubuntu/cassandra/apache-cassandra-2.0.5/conf/cassandra.yaml
+scp -i 391winter.pem conf/cassandra_node_2.yaml ubuntu@10.2.3.30:/home/ubuntu/cassandra/apache-cassandra-2.0.5/conf/cassandra.yaml
+scp -i 391winter.pem conf/cassandra_node_3.yaml ubuntu@10.2.3.5:/home/ubuntu/cassandra/apache-cassandra-2.0.5/conf/cassandra.yaml
+echo "Cassandra configuration uploaded to all noeds. Please restart Cassandra."
