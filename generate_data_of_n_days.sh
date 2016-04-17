677f1d74d62b765d72c36cece0eb79701decf756
Xiaohui Ma <xiaohui6@ualberta.ca>
2016-04-16 22:11:04 -0600

    upload latest version

--- generate_data_of_n_days.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Enter the number of days, followed by [ENTER]:"
-read days
-
-echo "Generating data of $days days..."
-ssh -i 391winter.pem ubuntu@10.2.3.31 "/home/ubuntu/generate $days"
--- keyspace_stats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# ssh -i 391winter.pem ubuntu@10.2.3.31 "/home/ubuntu/cassandra/apache-cassandra-2.0.5/bin/nodetool cfstats group4"
-
-ssh -i 391winter.pem ubuntu@10.2.3.31 "df -h | grep /dev/vdc"
-ssh -i 391winter.pem ubuntu@10.2.3.30 "df -h | grep /dev/vdc"
-ssh -i 391winter.pem ubuntu@10.2.3.5 "df -h | grep /dev/vdc"
